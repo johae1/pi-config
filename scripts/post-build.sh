4b089cfa373603d1aba78818020e603fd540d13f
Jonas Haese <haese.jonas@gmx.de>
2025-05-06 16:49:34 +0200

    added ntpd support

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,30 +1,31 @@
 #!/bin/sh
 
+echo "---------------------------------------------------------"
+
 # MARK A: Orginalskript starten
 echo "${TARGET_DIR}/../../board/raspberrypi4/post-build.sh $*"
 ${TARGET_DIR}/../../board/raspberrypi4/post-build.sh $*
 
 # MARK B: Netzwerk
-echo "-------------"
 echo "Placing interfaces file"
 install -m 755 ../target/interfaces $1/etc/network/
-echo "-------------"
 
 # MARK C: Zeit setzen
+install -m 755 ../target/ntp.conf $1/etc/
+install -m 755 ../target/S51ntpd $1/etc/init.d/
 
 # MARK D: SD-Karte einhaengen
 
 # MARK E: Webserver
-echo "-------------"
 echo "Installing webserver files"
 mkdir -p $1/var/www
 install -m 755    ../target/S51httpd   $1/etc/init.d/
 install           ../target/httpd.conf $1/etc/
 install -D        ../target/index.html $1/var/www/
 install -D -m 755 ../target/ps.cgi     $1/var/www/
-echo "-------------"
 
 # MARK F: Firewall
 
 # MARK G: Usermanagement
 
+echo "---------------------------------------------------------"
